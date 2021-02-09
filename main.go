66e1a70738987c542fa87b77e04f441247c666a4
KazushiMitsue <kazushi.mitsue9617@gmail.com>
2021-02-09 23:23:12 +0900

    sample cli

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "sampleApp"
+	app.Usage = "This app echo input arguments"
+	app.Version = "0.0.1"
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Hello!!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
