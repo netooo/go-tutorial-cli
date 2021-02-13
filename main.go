4dc230861daf19fe08d9767c9cb196c396d43aa5
netooo <kazushi.mitsue@gmail.com>
2021-02-13 18:39:43 +0900

    Add: command and option

--- main.go
+++ main.go
@@ -7,15 +7,41 @@ import (
 )
 
 func main() {
+	var (
+		suffix string
+	)
+
 	app := cli.NewApp()
 
 	app.Name = "sampleApp"
 	app.Usage = "This app echo input arguments"
 	app.Version = "0.0.1"
 
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello World")
-		return nil
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "suffix, s",
+			Value:       "!!!",
+			Usage:       "text after speaking something",
+			Destination: &suffix,
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "hello",
+			Usage: "if use set -t or --text",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "text, t",
+					Value: "world",
+					Usage: "hello xxx text",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				fmt.Printf("Hello %s %s\n", c.String("text"), suffix)
+				return nil
+			},
+		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
